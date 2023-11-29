f69031584a7f72aaa94010a51e1f97faf8eea61f
RussellMorryson <a1data@bk.ru>
2023-11-29 20:57:11 +0300

    Add last commit in 29.11.2023

--- main.cpp
+++ main.cpp
@@ -6,6 +6,6 @@ int main(){
     cout << "Hello world!" << endl;
     cout << "How are you?" << endl;
     cout << "Where are you from?" << endl;
-    
+    cout << "My name is Russell!" << endl;
     return 0;
 }
