059950ff8c60241862bd4720252ae5577c42729a
RussellMorryson <a1data@bk.ru>
2023-11-29 20:18:57 +0300

    Add third commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
     cout << "Hello world!" << endl;
-    cout << "How are you?" << endl;    
+    cout << "How are you?" << endl;
+    cout << "Where are you from?" << endl;
     return 0;
 }
